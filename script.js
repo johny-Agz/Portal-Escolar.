e73f4b735ec719e3a68a4cf10f4492dde92311fa
johny-Agz <jhonnygutierrezzambrana@gmail.com>
2025-02-15 01:48:42 -0600

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// Función para redirigir a las páginas de Profesor o Estudiante
-function redirectToPage(role) {
-    if (role === 'profesor') {
-        window.location.href = 'profesor.html'; // Redirige a la página de Profesor
-    } else if (role === 'estudiante') {
-        window.location.href = 'estudiante.html'; // Redirige a la página de Estudiante
-    }
+function redirigirAProfesor() {
+    window.location.href = "profesor.html";
+}
+
+function redirigirAEstudiante() {
+    window.location.href = "estudiante.html";
 }
