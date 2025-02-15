534f8d1c508b18af35647e82bb0a90d6ac8eeb5f
johny-Agz <jhonnygutierrezzambrana@gmail.com>
2025-02-14 23:21:17 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 function setRole(role) {
     if (role === "profesor") {
         window.location.href = "profesor.html"; // Redirige a la página de profesores
-    } else if (role === "estudiante") {
-        window.location.href = "estudiante.html"; // Redirige a la página de estudiantes
     } else {
         document.getElementById("menu").classList.remove("hidden");
         document.getElementById("profesor-tools").classList.add("hidden");
-        document.getElementById("estudiante-tools").classList.add("hidden");
     }
 }
